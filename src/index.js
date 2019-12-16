267281844c96e0b69149027360d724ef642739d0
Woo <leo9634@naver.com>
2019-12-16 06:08:28 -0500

    redux removed and try building again on the server

--- src/index.js
+++ src/index.js
@@ -9,9 +9,7 @@ import * as serviceWorker from './serviceWorker';
 
 
 ReactDOM.render(
-        <Provider store = {store}>
-          <App />
-        </Provider>,
+          <App />,
      document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
